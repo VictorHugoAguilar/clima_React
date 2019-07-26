936a0820dce3c6e00a5c9b1a574e6e8780ba8f7e
victor hugo <victor03016@gmail.com>
2019-07-26 19:44:52 +0200

    creacion del hook

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 // Importamos los componentes
 import Header from './Components/Header';
+import Formulario from './Components/Formulario';
+
 
 function App() {
   return (
@@ -11,6 +13,17 @@ function App() {
     <Header 
       titulo='Clima React App'
     />
+    <div className="contenedor-form">
+    <div className="container">
+      <div className="row">
+        <div className="col s12 m6">
+          <Formulario />
+        </div>
+
+      </div>
+    </div>
+
+    </div>
 
     </div>
   );
---
+++ src/Components/Formulario.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+function Formulario() {
+    // state del componente
+    // busqueda == al State
+    // guardar busqueda == this.setState({})
+    const [busqueda, guardarBusqueda] = useState({
+        ciudad: "",
+        pais: ""
+    });
+
+    const handleChange = e => {
+        // cambiar el state
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    return (
+        <form>
+            <div className="input-field col s12">
+                <input
+                    type="text"
+                    name="ciudad"
+                    id="ciudad"
+                    onChange={handleChange}
+                />
+                <label htmlFor="ciudad">Ciudad: </label>
+            </div>
+
+            <div className="input-fiel col s12">
+                <select onChange={handleChange} name="pais">
+                    <option value="">Selecciona un país</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="ES">España</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="FR">Francia</option>
+                    <option value="PE">Peru</option>
+                </select>
+            </div>
+
+            <div className="input-fiel col s12">
+                <input
+                    type="submit"
+                    className="waves-effect waves-light btn-large 
+                        btn-block yellow accent-4"
+                    value="Buscar Clima"
+                />
+            </div>
+        </form>
+    );
+}
+
+export default Formulario;
